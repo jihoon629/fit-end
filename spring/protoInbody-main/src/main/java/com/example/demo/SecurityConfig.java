bbe1e53d7e65ad886fd64b7dc8771387225ad005
Blue-B <source_vs@naver.com>
2025-03-14 19:00:23 +0900

    Feat:swager사용설정
    swagger+webui 사용하도록 설정하였습니다.
    
    [ui-web]
    http://YOUR-IP:8080/swagger-ui/index.html
    
    [json]
    http://YOUR-IP:8080/v3/api-docs

--- spring/protoInbody-main/src/main/java/com/example/demo/SecurityConfig.java
+++ spring/protoInbody-main/src/main/java/com/example/demo/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         .requestMatchers("/request/validate").authenticated()
                         .requestMatchers("/upload/register").permitAll()
                         .requestMatchers("/api/data").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/request/up").authenticated()
 
                         // 나머지 모든 요청은 인증 필요
